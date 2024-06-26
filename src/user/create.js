edc558d7b3791c5d11de4973e6a23d9900704566
Rehman Khan <rehman.khan.300a@gmail.com>
2024-06-26 21:39:47 +0530

    added free_user role and post limitations

--- src/user/create.js
+++ src/user/create.js
@@ -97,7 +97,7 @@ module.exports = function (User) {
 			db.incrObjectField('global', 'userCount'),
 			analytics.increment('registrations'),
 			db.sortedSetAddBulk(bulkAdd),
-			groups.join(['registered-users', 'unverified-users'], userData.uid),
+			groups.join(['registered-users', 'unverified-users', 'free-users'], userData.uid),
 			User.notifications.sendWelcomeNotification(userData.uid),
 			storePassword(userData.uid, data.password),
 			User.updateDigestSetting(userData.uid, meta.config.dailyDigestFreq),
